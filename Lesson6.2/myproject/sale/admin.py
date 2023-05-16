566ae4180a0eb62291689d4af7e4a59373da040e
starmoonn <114731908+starmoonn@users.noreply.github.com>
2023-05-16 19:51:54 +0700

    Delete Lesson6.2/myproject directory

--- Lesson6.2/myproject/sale/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .model import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price')
-    list_filter = ('price',)
-    search_fields = ('name', 'description')
-
-admin.site.register(Product, ProductAdmin)
--- Lesson6.2/myproject/sale/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductDetailView, ProductListView
-
-urlpatterns = [
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
-    path('products/', ProductListView.as_view(), name='product_list'),
-]
